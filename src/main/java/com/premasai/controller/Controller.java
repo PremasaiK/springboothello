4c8f5bcd67ebd8843bb39471b78d10099fb8af29
PremasaiK <81842544+PremasaiK@users.noreply.github.com>
2022-09-02 18:40:48 +0530

    Update Controller.java

--- src/main/java/com/premasai/controller/Controller.java
+++ src/main/java/com/premasai/controller/Controller.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping()
     public String fetchHelloWorld() {
-        return "Hello World";
+        return "Welcome to Softmax Solutions";
     }
 }
